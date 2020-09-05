0e588a5212a86b4036e6ac0e66cbf2c5698cbc97
Jubair Ahmed Junjun <51238714+Jubair-Ahmed-Junjun@users.noreply.github.com>
2020-09-05 09:16:58 +0600

    Update connect.php

--- php mysql database/connect.php
+++ php mysql database/connect.php
@@ -20,138 +20,115 @@
             echo "Error";
       }
 
+                         //update 
 
+$sql = "INSERT INTO users (first_name, last_name, email)
+VALUES ('John', 'Doe', 'john@example.com')";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                          //update 
-
-//       $sql = "INSERT INTO users (first_name, last_name, email)
-// VALUES ('John', 'Doe', 'john@example.com')";
-
-// if ($conn->query($sql) === TRUE) {
-//   $last_id = $conn->insert_id;
-//   echo "New record created successfully. Last inserted ID is: " . $last_id;
-// } else {
-//   echo "Error: " . $sql . "<br>" . $conn->error;
-// }
+if ($conn->query($sql) === TRUE) {
+  $last_id = $conn->insert_id;
+  echo "New record created successfully. Last inserted ID is: " . $last_id;
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 
                         
                              //Delete Data 
 
-      // $sql = "DELETE FROM users WHERE id = 7 ";
-      // if ($conn->query($sql)===TRUE) {
-      //       echo "Data Deteted Successfully";
-      // }else{
-      //       echo "Error";
-      // }
+      $sql = "DELETE FROM users WHERE id = 7 ";
+      if ($conn->query($sql)===TRUE) {
+            echo "Data Deteted Successfully";
+      }else{
+            echo "Error";
+      }
 
                                 
                                 //ORDER BY
-      // $sql = "SELECT id,first_name,last_name FROM users ORDER BY last_name";
-      // $result = $conn->query($sql);
-      // if ($result->num_rows>0) {
-      //       while ($row = $result->fetch_assoc()) {
+      $sql = "SELECT id,first_name,last_name FROM users ORDER BY last_name";
+      $result = $conn->query($sql);
+      if ($result->num_rows>0) {
+            while ($row = $result->fetch_assoc()) {
                   
-      //             echo "id : ".$row['id'] . " " . " Name :" . " ". $row['first_name']. " " . " " . $row['last_name']. " ";
-      //       }
-      // }else
-      // {
-      //       echo "Error";
-      // }
+                  echo "id : ".$row['id'] . " " . " Name :" . " ". $row['first_name']. " " . " " . $row['last_name']. " ";
+            }
+      }else
+      {
+            echo "Error";
+      }
 
 
 
                                   //WHERE
 
-      // $sql = "SELECT id,first_name,last_name,email FROM users WHERE last_name = 'rashid' ";
-      // $result = $conn->query($sql);
-      // if($result->num_rows > 0)
-      // {
-      //       while ($data = $result->fetch_assoc()) {
-      //             echo "id : ".$data['id']."".""."Name :"."".$data['first_name']."".$data['last_name'].""."Email : "."".$data['email'];
-      //       }
-      // }
-      // else{
-      //       echo "Error";
+      $sql = "SELECT id,first_name,last_name,email FROM users WHERE last_name = 'rashid' ";
+      $result = $conn->query($sql);
+      if($result->num_rows > 0)
+      {
+            while ($data = $result->fetch_assoc()) {
+                  echo "id : ".$data['id']."".""."Name :"."".$data['first_name']."".$data['last_name'].""."Email : "."".$data['email'];
+            }
+      }
+      else{
+            echo "Error";
       
      
                        //select
-      // $sql = "SELECT id,first_name,last_name,email FROM users";
-      // $result = $conn->query($sql);
-      // if ($result->num_rows > 0) {
-      // 	while ($row = $result->fetch_assoc()) {
-      // 		echo "id : ". $row['id']."-Name :"."".$row['first_name']." ".$row['last_name']." . ".$row['email']."<br>";
-      // 	}
-      // }else
-      // {
-      // 	echo "error";
-      // }
+           
+      $sql = "SELECT id,first_name,last_name,email FROM users";
+      $result = $conn->query($sql);
+      if ($result->num_rows > 0) {
+      	while ($row = $result->fetch_assoc()) {
+      		echo "id : ". $row['id']."-Name :"."".$row['first_name']." ".$row['last_name']." . ".$row['email']."<br>";
+      	}
+      }else
+      {
+      	echo "error";
+      }
 
 
 
 
                //Multiple Data insert
 
-      // $sql = "INSERT INTO users(first_name,last_name,email)VALUES('mamun','rashid','mamun@gmail.com'),('adi','rahman','adi@gmail.com'),('priam','boss','priam@gmail.com')";
-      // if($conn->query($sql)===TRUE)
-      // {
-      // 	echo "Multiple Data Insert Successfully";
-      // }else{
-      // 	echo "Error";
-      // }
+      $sql = "INSERT INTO users(first_name,last_name,email)VALUES('mamun','rashid','mamun@gmail.com'),('adi','rahman','adi@gmail.com'),('priam','boss','priam@gmail.com')";
+      if($conn->query($sql)===TRUE)
+      {
+      	echo "Multiple Data Insert Successfully";
+      }else{
+      	echo "Error";
+      }
 
 
                    // insert data
 
-      // $sql = "INSERT INTO users(first_name,last_name,email)VALUES('Emon','Alam','emon@gmail.com')";
-      // if($conn->query($sql)=== TRUE)
-      // {
-      // 	$last_id = $conn->insert_id;
-      // 	echo "New Record Created Successfully.New record is :".$last_id;
-      // }else{
-      // 	echo "Error";
-      // }
+      $sql = "INSERT INTO users(first_name,last_name,email)VALUES('Emon','Alam','emon@gmail.com')";
+      if($conn->query($sql)=== TRUE)
+      {
+      	$last_id = $conn->insert_id;
+      	echo "New Record Created Successfully.New record is :".$last_id;
+      }else{
+      	echo "Error";
+      }
                    
 
 
                  //Data Table
 
-      // $sql = "CREATE TABLE users(
-      // id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-      // first_name VARCHAR(255) NOT NULL,
-      // last_name VARCHAR(255) NOT NULL,
-      // email VARCHAR(255) NOT NULL
+      $sql = "CREATE TABLE users(
+      id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      first_name VARCHAR(255) NOT NULL,
+      last_name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL
 
-      //  )";
+       )";
 
-      //  if($conn->query($sql)===TRUE)
-      //  {
-      //  	echo "Data table created Successfully";
-      //  }else{
-      //  	echo "Failed to crating database";
-      //  }
+       if($conn->query($sql)===TRUE)
+       {
+       	echo "Data table created Successfully";
+       }else{
+       	echo "Failed to crating database";
+       }
 
                    
 
@@ -161,15 +138,15 @@
                   //Create Database
  
 
-      // $sql = "CREATE DATABASE student_info";
-      // if($conn->query($sql)===TRUE)
-      // {
-      // 	echo "Database Created Successfully";
-      // }
-      // else
-      // {
-      // 	echo "Failed to creating database";
-      //}
+      $sql = "CREATE DATABASE student_info";
+      if($conn->query($sql)===TRUE)
+      {
+      	echo "Database Created Successfully";
+      }
+      else
+      {
+      	echo "Failed to creating database";
+      }
 
 
 ?>
